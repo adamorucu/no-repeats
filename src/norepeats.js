12cbe9bd43a9952eab1f878035dd387cd5b5433d
Adam Orucu <adamdorucu@gmail.com>
2023-04-29 14:31:32 +0200

    Fixed issue with not wokring correctly on start

--- src/norepeats.js
+++ src/norepeats.js
@@ -1,4 +1,44 @@
 
+// function isOnMainPage() {
+//   const url = window.location.href;
+//   const mainPagePattern = /^https:\/\/www\.youtube\.com\/$/;
+//   return mainPagePattern.test(url);
+// }
+
+// function hideWatchedVideos(isEnabled) {
+//   if (isOnMainPage()) {
+
+//     const videoThumbnails = document.querySelectorAll('#dismissible');
+
+//     videoThumbnails.forEach((thumbnail) => {
+//       const progress = thumbnail.querySelector('#progress');
+//       if (progress && parseInt(progress.style.width) > 70) {
+//         thumbnail.style.display = 'none';
+//       }
+//     });
+//   } else {
+//     return;
+//   }
+// }
+
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === 'toggleEnabled') {
+//     hideWatchedVideos(request.isEnabled);
+//   }
+// });
+
+
+// // Run the function on page load
+// hideWatchedVideos(isEnabled);
+
+// // Run the function when the page's DOM is updated
+// const observer = new MutationObserver(() => {
+//   hideWatchedVideos();
+// });
+
+// observer.observe(document.body, { childList: true, subtree: true });
+
 function isOnMainPage() {
   const url = window.location.href;
   const mainPagePattern = /^https:\/\/www\.youtube\.com\/$/;
@@ -6,8 +46,7 @@ function isOnMainPage() {
 }
 
 function hideWatchedVideos(isEnabled) {
-  if (isOnMainPage()) {
-
+  if (isEnabled && isOnMainPage()) {
     const videoThumbnails = document.querySelectorAll('#dismissible');
 
     videoThumbnails.forEach((thumbnail) => {
@@ -21,20 +60,24 @@ function hideWatchedVideos(isEnabled) {
   }
 }
 
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'toggleEnabled') {
     hideWatchedVideos(request.isEnabled);
   }
 });
 
+// Helper function to get isEnabled value and run hideWatchedVideos
+async function runWithIsEnabled() {
+  const response = await browser.runtime.sendMessage({ action: 'getIsEnabled' });
+  hideWatchedVideos(response.isEnabled);
+}
 
 // Run the function on page load
-hideWatchedVideos();
+runWithIsEnabled();
 
 // Run the function when the page's DOM is updated
 const observer = new MutationObserver(() => {
-  hideWatchedVideos();
+  runWithIsEnabled();
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
